74f2b4fb0009fb0ef2da6fff36fe0710b147482e
Leandro Ezequiel Danko <100171997+ldanko91@users.noreply.github.com>
2022-12-13 22:42:05 -0300

    preentrega-1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-
 const router = Router();
 
 const products = [
@@ -13,7 +12,7 @@ const products = [
       },
     {
         "title": "Vanitory Maral 60",
-        "price": 21000,
+        "price": 27000,
         "thumbnail": "./img/maral60.jpg",
         "id": 2
       },
@@ -26,34 +25,90 @@ const products = [
       
 ]
 
-router.get("/product/:id", (req, res)=> {
-    const {id} = req.params;
-        const product = products.find(product=> product.id === Number(id));
+const isAdmin = true;
 
-        res.render("product", product);
-    });
+router.get("/api/productos/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((product) => product.id === Number(id));
 
-router.get("/product", (req, res)=> {
-    res.render("products", { products, hasAny: true });
-    });
-
-router.get("/", (req, res) => {
-    res.render("productForm")
+  res.render("product", product);
 });
 
-router.post('/', (req, res) => {
-    const { name, price, thumbnail } = req.body;
-    const maxId = products.length;
-
-    products.push({ title:name , price:price , thumbnail:thumbnail , id: maxId + 1});
-    console.log(products)
-
-
-    res.redirect("/");
+router.get("/api/productos", (req, res) => {
+  res.render("products", { products, hasAny: true });
 });
 
-router.get("/product", (req, res) => {
-    res.render("products", { products });
+isAdmin
+  ? router.get("/api/productos/carga", (req, res) => {
+      res.render("productForm");
+    })
+  : router.get("/api/productos/carga", (req, res) => {
+      res.render("notAdmin");
+    });
+
+isAdmin
+  ? router.post("/api/productos/carga", (req, res) => {
+      const { name, price, thumbnail } = req.body;
+      const maxId = products.length;
+
+      products.push({
+        title: name,
+        price: price,
+        thumbnail: thumbnail,
+        id: maxId + 1,
+      });
+      console.log(products);
+
+      res.redirect("/api/productos");
+    })
+  : router.post("/carga", (req, res) => {
+      res.render("notAdmin");
+    });
+
+isAdmin
+? router.get("/api/productos/editar", (req, res) => {
+    res.render("putForm");
+  })
+: router.get("/api/productos/editar", (req, res) => {
+    res.render("notAdmin");
+  });
+  
+
+  isAdmin
+?  router.put("/api/productos/editar", (req, res) => {
+    const {id} = req.body.id;
+    const updateProd = req.body;
+    
+    products.splice((Number(id)-1), 1)
+
+    products.push({ title:updateProd.name,
+                    price:updateProd.price,
+                    thumbnail:updateProd.thumbnail,
+                    id: Number(id)});
+    console.log(updateProd)
+
+    res.redirect("/api/productos/editar");
+})
+
+: router.put("/api/productos/editar", (req, res) => {
+  res.render("notAdmin");
+});
+
+isAdmin
+?  router.delete("/api/productos/:id", (req, res) => {
+  const { id } = req.params;
+  products.splice((Number(id)-1), 1);
+
+  res.render("product", product);
+})
+
+: router.delete("/api/productos/:id", (req, res) => {
+  res.render("notAdmin");
 });
 
 export default router;
+
+
+// router.get("/product", (req, res) => {
+//   res.render("products", { products });
+// });
